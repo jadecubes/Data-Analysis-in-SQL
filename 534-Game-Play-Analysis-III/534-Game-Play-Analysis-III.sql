86a464c345ef1c86ea620b9adb6412ef067bd549
jadecubes <debra321@hotmail.com>
2024-05-01 16:45:32 +0800

    Update 534-Game-Play-Analysis-III.sql

--- 534-Game-Play-Analysis-III/534-Game-Play-Analysis-III.sql
+++ 534-Game-Play-Analysis-III/534-Game-Play-Analysis-III.sql
@@ -1,24 +1,6 @@
--- By PARTITION
-WITH CTE AS (
-    SELECT
-        player_id,
-        device_id,
-        ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS rn
-    FROM Activity
-)
-SELECT player_id, device_id
-FROM CTE
-WHERE rn = 1;
-
-
--- BY GROUP BY
-WITH CTE AS (
-    SELECT
-        player_id,
-        MIN(event_date) AS min_event_date,
-        device_id
-    FROM Activity
-    GROUP BY player_id, device_id
-)
-SELECT CTE.player_id, CTE.device_id
-FROM CTE;
+SELECT 
+  player_id, 
+  event_date, 
+  SUM(games_played) OVER (PARTITION BY player_id ORDER BY event_date) AS games_played_so_far
+FROM Activity
+ORDER BY player_id, event_date;
