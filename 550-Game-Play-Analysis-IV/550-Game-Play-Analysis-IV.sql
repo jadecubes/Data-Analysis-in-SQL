a14d457eb7fcc3dda3d08782489dc2441b7cddcb
jadecubes <debra321@hotmail.com>
2024-05-03 02:07:51 +0800

    Update 550-Game-Play-Analysis-IV.sql

--- 550-Game-Play-Analysis-IV/550-Game-Play-Analysis-IV.sql
+++ 550-Game-Play-Analysis-IV/550-Game-Play-Analysis-IV.sql
@@ -1,24 +1,19 @@
--- By PARTITION
-WITH CTE AS (
+WITH FirstLogins AS (
     SELECT
         player_id,
-        device_id,
-        ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS rn
+        MIN(event_date) AS first_login
     FROM Activity
-)
-SELECT player_id, device_id
-FROM CTE
-WHERE rn = 1;
-
-
--- BY GROUP BY
-WITH CTE AS (
+    GROUP BY player_id
+),
+SecondDayLogins AS (
     SELECT
-        player_id,
-        MIN(event_date) AS min_event_date,
-        device_id
-    FROM Activity
-    GROUP BY player_id, device_id
+        f.player_id
+    FROM FirstLogins f
+    JOIN Activity a ON f.player_id = a.player_id
+        AND  a.event_date = DATE_ADD(f.first_login)
+    GROUP BY f.player_id
 )
-SELECT CTE.player_id, CTE.device_id
-FROM CTE;
+SELECT
+    ROUND(COUNT(DISTINCT s.player_id) / CAST(COUNT(DISTINCT f.player_id) AS FLOAT), 2) AS fraction
+FROM FirstLogins f
+LEFT JOIN SecondDayLogins s ON f.player_id = s.player_id;
