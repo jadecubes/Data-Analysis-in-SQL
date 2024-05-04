d5b14df6fc099c8fe84673de7d48ab33ab9d2863
jadecubes <debra321@hotmail.com>
2024-05-04 20:43:01 +0800

    Update 512-Game-Play-Analysis-II.sql

--- 512-Game-Play-Analysis-II/512-Game-Play-Analysis-II.sql
+++ 512-Game-Play-Analysis-II/512-Game-Play-Analysis-II.sql
@@ -10,15 +10,3 @@ SELECT player_id, device_id
 FROM CTE
 WHERE rn = 1;
 
-
--- BY GROUP BY
-WITH CTE AS (
-    SELECT
-        player_id,
-        MIN(event_date) AS min_event_date,
-        device_id
-    FROM Activity
-    GROUP BY player_id, device_id
-)
-SELECT CTE.player_id, CTE.device_id
-FROM CTE;
